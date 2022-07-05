46ddc5d239d32db73c6f6b249e02a96e889e5c8b
malkaviano <rafael.kss@gmail.com>
2022-07-05 17:27:17 -0300

    chore: add material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lamestgame'`, () => {
+  it(`should have as title 'lamest-game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lamestgame');
+    expect(app.title).toEqual('lamest-game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lamestgame app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'lamest-game app is running!'
+    );
   });
 });
