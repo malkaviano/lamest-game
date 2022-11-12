c6463e258a60e4a42fd3243d92438c016486ae8a
malkaviano <rafael.kss@gmail.com>
2022-11-12 17:36:55 -0300

    test: add random generation check

--- src/app/services/random-int.service.spec.ts
+++ src/app/services/random-int.service.spec.ts
@@ -13,4 +13,22 @@ describe('RandomIntService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should generate numbers between 0 and 100', () => {
+    const results: any = {};
+
+    for (let index = 0; index < 500; index++) {
+      const element = service.getRandomInterval(0, 100);
+
+      results[element] = true;
+    }
+
+    let final = true;
+
+    for (const key in results) {
+      final = final && (results[key] ?? false);
+    }
+
+    expect(final).toBe(true);
+  });
 });
