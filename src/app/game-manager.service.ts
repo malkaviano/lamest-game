30395dc8d511f18c9c52085d35c07bd72f80c0ed
malkaviano <rafael.kss@gmail.com>
2022-11-15 09:44:35 -0300

    feat: main layout

--- src/app/game-manager.service.ts
+++ src/app/game-manager.service.ts
@@ -27,12 +27,65 @@ export class GameManagerService {
     new Scene(
       'scene1',
       [
-        'You are in you room.',
-        'The bed is unmade.',
-        "There're leftovers from yesterday over the table",
-        'And your mom is calling you downstairs',
+        `O veículo de vocês parece acelerar um pouco mais e saltar um pouco menos. A estrada rústica dá lugar a uma rodovia asfaltada e mais luzes parecem se aproximar de vocês. Luzes de uma cidade que parece estar viva no cair da noite. A van atravessa o pórtico da cidade e é possível ler o letreiro que os recebe: “Boas vindas a Aurora, a cidade das flores”.`,
+        `Algumas pessoas que caminham pelas ruas olham para vocês com um ar de curiosidade. Já outras olham com desconfiança. De uma forma ou de outra, existe uma certeza: vocês não são parte dessa cidade. São estranhos. Os olhares se intensificam quando vocês se aproximam da delegacia, onde Caio combinou de se encontrar com Larissa.`,
+        `A investigadora os espera na porta da delegacia. Larissa Ribeiro é uma mulher jovem e magra, de óculos e cabelos presos num coque. Seu olhar é de preocupação. O uniforme da polícia está ligeiramente, escondido debaixo de um casaco mais grosso, para escapar do clima frio da cidade.`,
       ],
       [
+        new InteractiveEntity(
+          'bed1',
+          'BED',
+          'Making your bed',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'food1',
+          'LEFTOVERS',
+          'Food?',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'exit1',
+          'DOWNSTAIRS',
+          'Leave',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'bed1',
+          'BED',
+          'Making your bed',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'food1',
+          'LEFTOVERS',
+          'Food?',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'exit1',
+          'DOWNSTAIRS',
+          'Leave',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'bed1',
+          'BED',
+          'Making your bed',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'food1',
+          'LEFTOVERS',
+          'Food?',
+          new OpenedContainerState(new ArrayView([]))
+        ),
+        new InteractiveEntity(
+          'exit1',
+          'DOWNSTAIRS',
+          'Leave',
+          new OpenedContainerState(new ArrayView([]))
+        ),
         new InteractiveEntity(
           'bed1',
           'BED',
