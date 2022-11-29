9981be808bb16b2319da8572d9a706c926bf547a
malkaviano <rafael.kss@gmail.com>
2022-11-29 10:39:16 -0300

    feat: add scene store from json

--- src/app/stores/scene.store.ts
+++ src/app/stores/scene.store.ts
@@ -1,50 +1,52 @@
 import { Injectable } from '@angular/core';
 
 import { SceneEntity } from '../entities/scene.entity';
+import { ConverterHelper } from '../helpers/converter.helper';
 import { KeyValueInterface } from '../interfaces/key-value.interface';
 import { ArrayView } from '../views/array.view';
 import { DescriptionStore } from './description.store';
 import { InteractiveStore } from './interactive.store';
 
+import sceneStore from '../../assets/scenes.json';
+
 @Injectable({
   providedIn: 'root',
 })
 export class SceneStore {
-  public readonly scenes: KeyValueInterface<SceneEntity>;
+  private readonly store: Map<string, SceneEntity>;
 
   constructor(
     public readonly descriptionsStore: DescriptionStore,
-    public readonly interactiveStore: InteractiveStore
+    public readonly interactiveStore: InteractiveStore,
+    private readonly converterHelper: ConverterHelper
   ) {
-    this.scenes = {
-      scene1: new SceneEntity(
-        this.descriptionsStore.descriptions['scene1'],
-        new ArrayView([
-          this.interactiveStore.interactives['npc1'],
-          this.interactiveStore.interactives['sceneExitDoor'],
-          this.interactiveStore.interactives['upperShelf'],
-          this.interactiveStore.interactives['woodBox'],
-          this.interactiveStore.interactives['corridorDoor'],
-          this.interactiveStore.interactives['table'],
-        ]),
-        { sceneExitDoor: 'scene2', corridorDoor: 'scene3' }
-      ),
-      scene2: new SceneEntity(
-        this.descriptionsStore.descriptions['scene2'],
-        new ArrayView([
-          this.interactiveStore.interactives['enterSceneDoor'],
-          this.interactiveStore.interactives['trainingDummy'],
-        ]),
-        { enterSceneDoor: 'scene1' }
-      ),
-      scene3: new SceneEntity(
-        this.descriptionsStore.descriptions['scene3'],
-        new ArrayView([
-          this.interactiveStore.interactives['enterSceneDoor'],
-          this.interactiveStore.interactives['zombie'],
-        ]),
-        { enterSceneDoor: 'scene1' }
-      ),
-    };
+    this.store = new Map<string, SceneEntity>();
+
+    sceneStore.scenes.forEach((scene) => {
+      const interactives = scene.interactives.map((id) => {
+        return this.interactiveStore.interactives[id];
+      });
+
+      const transitions = scene.transitions.reduce(
+        (obj: any, { name, scene }) => {
+          obj[name] = scene;
+
+          return obj;
+        },
+        {}
+      );
+      this.store.set(
+        scene.name,
+        new SceneEntity(
+          this.descriptionsStore.descriptions[scene.description],
+          new ArrayView(interactives),
+          transitions
+        )
+      );
+    });
+  }
+
+  public get scenes(): KeyValueInterface<SceneEntity> {
+    return this.converterHelper.mapToKeyValueInterface(this.store);
   }
 }
