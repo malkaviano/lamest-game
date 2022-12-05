150410d71c1fdf9e9f903abd4d5892d5a44be721
malkaviano <rafael.kss@gmail.com>
2022-12-05 15:16:19 -0300

    feat: add actor behavior

---
+++ src/app/behaviors/actor.behavior.spec.ts
@@ -0,0 +1,52 @@
+import { CharacteristicSetDefinition } from '../definitions/characteristic-set.definition';
+import { CharacteristicDefinition } from '../definitions/characteristic.definition';
+import { DerivedAttributeSetDefinition } from '../definitions/derived-attribute-set.definition';
+import { DerivedAttributeDefinition } from '../definitions/derived-attribute.definition';
+import { SkillNameLiteral } from '../literals/skill-name.literal';
+import { ActorBehavior } from './actor.behavior';
+
+describe('ActorBehavior', () => {
+  describe('characteristics', () => {
+    it('return characteristics', () => {
+      expect(behavior.characteristics).toEqual(expectedCharacteristics);
+    });
+  });
+
+  describe('derived attributes', () => {
+    it('return derived attributes', () => {
+      expect(behavior.derivedAttributes).toEqual(expectedDerivedAttributes);
+    });
+  });
+
+  describe('skills', () => {
+    it('return skills with characteristics applied', () => {
+      expect(behavior.skills).toEqual({
+        Appraise: 12,
+        Dodge: 32,
+      });
+    });
+  });
+});
+
+const expectedCharacteristics: CharacteristicSetDefinition = {
+  STR: new CharacteristicDefinition('STR', 8),
+  CON: new CharacteristicDefinition('CON', 9),
+  SIZ: new CharacteristicDefinition('SIZ', 10),
+  DEX: new CharacteristicDefinition('DEX', 11),
+  INT: new CharacteristicDefinition('INT', 12),
+  POW: new CharacteristicDefinition('POW', 13),
+  APP: new CharacteristicDefinition('APP', 14),
+};
+
+const expectedDerivedAttributes: DerivedAttributeSetDefinition = {
+  HP: new DerivedAttributeDefinition('HP', 9),
+  PP: new DerivedAttributeDefinition('PP', 13),
+  MOV: new DerivedAttributeDefinition('MOV', 10),
+};
+
+const expectedSkills = new Map<SkillNameLiteral, number>([
+  ['Appraise', 0],
+  ['Dodge', 10],
+]);
+
+const behavior = new ActorBehavior(expectedCharacteristics, expectedSkills);
---
+++ src/app/behaviors/actor.behavior.ts
@@ -0,0 +1,58 @@
+import { CharacteristicSetDefinition } from '../definitions/characteristic-set.definition';
+import { DerivedAttributeSetDefinition } from '../definitions/derived-attribute-set.definition';
+import { DerivedAttributeDefinition } from '../definitions/derived-attribute.definition';
+import { skillDefinitions } from '../definitions/skill.definition';
+import { ActorInterface } from '../interfaces/actor.interface';
+import { KeyValueInterface } from '../interfaces/key-value.interface';
+import { SkillNameLiteral } from '../literals/skill-name.literal';
+
+export class ActorBehavior implements ActorInterface {
+  private readonly maximumHP: number;
+
+  private readonly maximumPP: number;
+
+  private currentHP: number;
+
+  private currentPP: number;
+
+  constructor(
+    private readonly mCharacteristics: CharacteristicSetDefinition,
+    private readonly mSkills: Map<SkillNameLiteral, number>
+  ) {
+    this.maximumHP = Math.trunc(
+      (this.characteristics['CON'].value + this.characteristics['SIZ'].value) /
+        2
+    );
+
+    this.maximumPP = this.characteristics['POW'].value;
+
+    this.currentHP = this.maximumHP;
+
+    this.currentPP = this.maximumPP;
+  }
+
+  get characteristics(): CharacteristicSetDefinition {
+    return Object.assign({}, this.mCharacteristics);
+  }
+
+  get derivedAttributes(): DerivedAttributeSetDefinition {
+    return {
+      HP: new DerivedAttributeDefinition('HP', this.currentHP),
+      PP: new DerivedAttributeDefinition('PP', this.currentPP),
+      MOV: new DerivedAttributeDefinition('MOV', 10),
+    };
+  }
+
+  get skills(): KeyValueInterface<number> {
+    return Array.from(this.mSkills.entries()).reduce(
+      (acc: { [key: string]: number }, [k, v]) => {
+        const base = skillDefinitions[k].base(this.characteristics);
+
+        acc[k] = v + base;
+
+        return acc;
+      },
+      {}
+    );
+  }
+}
