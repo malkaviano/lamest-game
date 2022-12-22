1eb8b2a353bd5bd3bba4773cddcc5146ed56fd9c
malkaviano <rafael.kss@gmail.com>
2022-12-22 11:18:22 -0300

    test: fix pick rule specs

--- src/app/rules/pick.rule.spec.ts
+++ src/app/rules/pick.rule.spec.ts
@@ -5,6 +5,8 @@ import { deepEqual, instance, verify, when } from 'ts-mockito';
 import { InventoryService } from '../services/inventory.service';
 import { PickRule } from './pick.rule';
 import { StringMessagesStoreService } from '../stores/string-messages.store.service';
+import { LogMessageDefinition } from '../definitions/log-message.definition';
+import { ExtractorHelper } from '../helpers/extractor.helper';
 
 import {
   mockedExtractorHelper,
@@ -21,8 +23,7 @@ import {
   playerInfo,
   simpleSword,
 } from '../../../tests/fakes';
-import { ExtractorHelper } from '../helpers/extractor.helper';
-import { LogMessageDefinition } from '../definitions/log-message.definition';
+import { ruleScenario } from '../../../tests/scenarios';
 
 describe('PickRule', () => {
   let service: PickRule;
@@ -47,26 +48,63 @@ describe('PickRule', () => {
 
     setupMocks();
 
-    when(
-      mockedStringMessagesStoreService.createTookLogMessage(
-        playerInfo.name,
-        interactiveInfo.name,
-        simpleSword.identity.label
-      )
-    ).thenReturn(log);
-
     service = TestBed.inject(PickRule);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('execute', () => {
+    it('should log item picked', () => {
+      when(
+        mockedExtractorHelper.extractItemOrThrow(
+          instance(mockedInventoryService),
+          eventPickSimpleSword.eventId,
+          eventPickSimpleSword.actionableDefinition.name
+        )
+      ).thenReturn(simpleSword);
+
+      when(
+        mockedInteractiveEntity.reactTo(
+          eventPickSimpleSword.actionableDefinition,
+          'NONE',
+          deepEqual({})
+        )
+      ).thenReturn(itemTookMessage);
+
+      when(
+        mockedStringMessagesStoreService.createTookLogMessage(
+          playerInfo.name,
+          interactiveInfo.name,
+          itemTookMessage
+        )
+      ).thenReturn(itemTookLog);
+
+      ruleScenario(
+        service,
+        actor,
+        eventPickSimpleSword,
+        {
+          target: instance(mockedInteractiveEntity),
+        },
+        [itemTookLog]
+      );
+
+      // Cheap side effect check, instead of another test case
+      verify(mockedInventoryService.store(actor.id, simpleSword)).once();
+    });
+  });
 });
 
-const log = new LogMessageDefinition(
+const actor = instance(mockedPlayerEntity);
+
+const itemTookMessage = `${interactiveInfo.name}-${simpleSword.identity.label}`;
+
+const itemTookLog = new LogMessageDefinition(
   'TOOK',
   playerInfo.name,
-  `${interactiveInfo.name}-${simpleSword.identity.label}`
+  itemTookMessage
 );
 
 const eventPickSimpleSword = actionableEvent(
