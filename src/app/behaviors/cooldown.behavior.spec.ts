3bea1976a2bbe15de2f221cd7e9fec5a1e67ed1e
malkaviano <rafael.kss@gmail.com>
2022-12-26 10:28:34 -0300

    test: cooldown behavior spec

--- src/app/behaviors/cooldown.behavior.spec.ts
+++ src/app/behaviors/cooldown.behavior.spec.ts
@@ -1,7 +1,21 @@
+import { fakeAsync, tick } from '@angular/core/testing';
+
 import { CooldownBehavior } from './cooldown.behavior';
 
 describe('CooldownBehavior', () => {
-  it('should create an instance', () => {
-    expect(CooldownBehavior.create(2000)).toBeTruthy();
+  describe('canAct', () => {
+    it('return true then false', fakeAsync(() => {
+      expect(behavior.canAct).toEqual(true);
+
+      expect(behavior.canAct).toEqual(false);
+
+      tick(500);
+
+      expect(behavior.canAct).toEqual(true);
+
+      tick(1000);
+    }));
   });
 });
+
+const behavior = CooldownBehavior.create(500);
