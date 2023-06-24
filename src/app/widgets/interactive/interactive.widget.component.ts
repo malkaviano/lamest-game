760521073c057b877bc6de7a29429cc7984afa82
Rafael Karayannopoulos <rafael.kss@gmail.com>
2023-06-24 10:42:37 -0300

    feat: add icons tooltips

--- src/app/widgets/interactive/interactive.widget.component.ts
+++ src/app/widgets/interactive/interactive.widget.component.ts
@@ -12,6 +12,7 @@ import { ActionableEvent } from '../../../core/events/actionable.event';
 import { ArrayView } from '../../../core/view-models/array.view';
 import { InteractiveInterface } from '../../../core/interfaces/interactive.interface';
 import { WithSubscriptionHelper } from '../../helpers/with-subscription.helper';
+import { GameStringsStore } from '../../../stores/game-strings.store';
 
 @Component({
   selector: 'app-interactive-widget',
@@ -24,9 +25,21 @@ export class InteractiveWidgetComponent implements OnInit, OnDestroy {
   @Output() actionSelected: EventEmitter<ActionableEvent>;
   actions: ArrayView<ActionableDefinition>;
 
+  public readonly aggressiveTooltip: String;
+  public readonly retaliateTooltip: String;
+  public readonly playerTooltip: String;
+  public readonly searchTooltip: String;
+  public readonly visibilityTooltip: String;
+
   constructor(private readonly withSubscriptionHelper: WithSubscriptionHelper) {
     this.actionSelected = new EventEmitter<ActionableEvent>();
     this.actions = ArrayView.create([]);
+
+    this.aggressiveTooltip = GameStringsStore.tooltips['aggressive'];
+    this.retaliateTooltip = GameStringsStore.tooltips['retaliate'];
+    this.playerTooltip = GameStringsStore.tooltips['retaliate'];
+    this.searchTooltip = GameStringsStore.tooltips['search'];
+    this.visibilityTooltip = GameStringsStore.tooltips['visibility'];
   }
 
   ngOnInit(): void {
