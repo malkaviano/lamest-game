32934a2d109c6108f0146974d9d214a12fcb4e2b
Rafael Karayannopoulos <rafael.kss@gmail.com>
2023-07-21 11:11:22 -0300

    change: filter scene empty interactives

--- src/backend/entities/scene.entity.ts
+++ src/backend/entities/scene.entity.ts
@@ -2,12 +2,20 @@ import { ArrayView } from '@wrappers/array.view';
 import { InteractiveInterface } from '@interfaces/interactive.interface';
 
 export class SceneEntity {
+  private readonly mInteractives: ArrayView<InteractiveInterface>;
+
   constructor(
     public readonly name: string,
     public readonly label: string,
-    public readonly interactives: ArrayView<InteractiveInterface>,
+    interactives: ArrayView<InteractiveInterface>,
     public readonly image: string
-  ) {}
+  ) {
+    this.mInteractives = interactives;
+  }
+
+  public get interactives(): ArrayView<InteractiveInterface> {
+    return this.mInteractives.filter((i) => i.actions.items.length > 0);
+  }
 
   public reset(): void {
     this.interactives.items.forEach((i) => i.reset());
